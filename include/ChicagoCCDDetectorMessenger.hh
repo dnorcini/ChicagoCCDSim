727597242ff4568a127bec00b805b0c08442289e
Sputnicktator <ncorso@uchicago.edu>
2020-08-26 15:24:40 -0500

    Made the two CCD models swappable via UI commands.

--- include/ChicagoCCDDetectorMessenger.hh
+++ include/ChicagoCCDDetectorMessenger.hh
@@ -2,12 +2,15 @@
 #define ChicagoCCDDetectorMessenger_h 1
 
 #include "globals.hh"
+#include "G4UIcmdWithoutParameter.hh"
+#include "G4UIcmdWithABool.hh"
 #include "G4UImessenger.hh"
 
 class ChicagoCCDDetectorConstruction;
 class G4UIdirectory;
 class G4UIcommand;
 class G4UIcmdWithoutParameter;
+class G4UIcmdWithABool;
 
 class ChicagoCCDDetectorMessenger: public G4UImessenger
 {
@@ -22,6 +25,7 @@ class ChicagoCCDDetectorMessenger: public G4UImessenger
 
     G4UIdirectory* CommandDir;
     G4UIcmdWithoutParameter* DelGeomCmd;
+    G4UIcmdWithABool* CCDDeadCmd;
 };
 
 #endif
